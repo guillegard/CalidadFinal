3e14aacb119e4bb794238ffecb8868ffd73460a0
guillegard <guillegard95@gmail.com>
2019-05-15 10:58:49 -0500

    Updates

---
+++ precache-manifest.adbd16020910ffddab1e6cb7a94e0344.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "c898afdc5d5178edddecec3b1d2cd176",
+    "url": "/CalidadFinal/static/media/trash.c898afdc.svg"
+  },
+  {
+    "revision": "af4b18ad09575ee11d0b928ea5233e01",
+    "url": "/CalidadFinal/static/media/pen.af4b18ad.svg"
+  },
+  {
+    "revision": "c15f46010a7f54ce6cc0109fe3bc070f",
+    "url": "/CalidadFinal/static/media/image.c15f4601.png"
+  },
+  {
+    "revision": "ecac6caff098aa88a80884c358a71fa9",
+    "url": "/CalidadFinal/static/media/groupBack.ecac6caf.png"
+  },
+  {
+    "revision": "898be9acf4e581d730ec",
+    "url": "/CalidadFinal/static/js/runtime~main.898be9ac.js"
+  },
+  {
+    "revision": "3077dbfc58647ae4368a",
+    "url": "/CalidadFinal/static/js/main.3077dbfc.chunk.js"
+  },
+  {
+    "revision": "02fec7a929b40372cea2",
+    "url": "/CalidadFinal/static/js/1.02fec7a9.chunk.js"
+  },
+  {
+    "revision": "3077dbfc58647ae4368a",
+    "url": "/CalidadFinal/static/css/main.a19a4bcd.chunk.css"
+  },
+  {
+    "revision": "02fec7a929b40372cea2",
+    "url": "/CalidadFinal/static/css/1.a7a885fa.chunk.css"
+  },
+  {
+    "revision": "e6acd03cb016a0e8a6e1f93326c63920",
+    "url": "/CalidadFinal/index.html"
+  }
+];
