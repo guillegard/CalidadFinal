ea15da58b7aa68e6de1d1b2872e8a1cd02557ee2
guillegard <guillegard95@gmail.com>
2019-05-15 02:55:54 -0500

    jenkins test 4

--- src/App.test.js
+++ src/App.test.js
@@ -8,3 +8,8 @@ it('renders without crashing', () => {
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('sums numbers', () => {
+  expect(1 + 2).toEqual(3);
+  expect(2 + 2).toEqual(4);
+});
